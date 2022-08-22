a196da44cd4dd4eb7aa42fd145bdfd31d8c7b933
Anto <107312386+zioForas@users.noreply.github.com>
2022-08-23 00:05:17 +0100

    first commit

---
+++ libr/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Landing from './components/Landing'
+import Highlights from './components/Highlights'
+import Featured from './components/Featured'
+import Discounted from './components/Discounted'
+import Explore from './components/Explore'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <div className="App">
+<Nav/>
+<Landing />
+<Highlights/>
+<Featured />
+<Discounted />
+<Explore/>
+<Footer/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ libr/src/components/Highlights.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Highlight from "./ui/Highlight";
+
+const Highlights = () => {
+  return (
+    <section id="highlights">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Why Choose <span className="purple">Library</span>
+          </h2>
+          <div className="highlight__wrapper">
+            <Highlight
+              icon={<FontAwesomeIcon icon="bolt" />}
+              title="Easy and Quick"
+              para="Get access to the book you purchased online instalnly."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="book-open" />}
+              title="10,000 books"
+              para="Library has books in all your favourite categories"
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="tags" />}
+              title="Affordable"
+              para="Get your hands on your favourite books for as little as $10 "
+            />
+          
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
