c456f808d3c67a60bdba936cbe1d3eaab3e58b46
Anto <107312386+zioForas@users.noreply.github.com>
2022-08-24 22:19:11 +0100

    nice

--- libr/src/App.jsx
+++ libr/src/App.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router,  Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  //  function addToCart(book){//
+  //   const dupeItem = cart.find(item => +item.id === +book.id)
+  //   if (dupeItem){
+  //     setCart(cart.map(item => {
+  //       if  (item.id === dupeItem.id) {
+  //         return {
+  //           ...item,
+  //           quantity: item.quantity + 1,
+// apparently too complicated
+  //         }
+  //       }
+  //       else {
+  //         return item
+  //       }
+  //     }))
+  //   }
+  //   else {
+  //     setCart([...cart, {...book, quantity: 1 }])
+  //   }
+  //  }
+
+  // useEffect(() =>{
+  //    console.log(cart,);
+  // }, [cart])
+  function addToCart(book){
+    setCart([...cart, book])
+  }
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
   return (
     <Router>
       <div className="App">
         <Nav />
          <Route path="/" exact component={Home}/> 
-         <Route path="/books" render={() => <Books books={books} />} />
-        {/* <Route path="/" exact render={() => <Home books={Books} />} />
-        <Route path="/books" exact render={() => <Books books={Books} />} />  */}
-
-        {/* <Route path="/books/:id" element={<BookInfo books={books} 
-        addToCart={addToCart} cart={cart}/> } />
-        <Route path="/cart" element={<Cart books={books} cart={cart} 
-        changeQuantity={changeQuantity} />} /> */}
-        
+         <Route path="/books" exact render={() => <Books books={books} />} />
+         <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart}/>} />
+        <Route path="/cart" render={() => <Cart books={books} />} />  
         
         <Footer />
       </div>
